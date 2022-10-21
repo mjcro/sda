57ec1c5baa2d213e54841d8f2e34d1a328bc43cd
gotterdemarung <gotterdemarung@gmail.com>
2022-10-21 18:52:47 +0300

    :memo: AbstractSqlRepository Javadoc

--- src/main/java/io/github/mjcro/sda/AbstractSqlRepository.java
+++ src/main/java/io/github/mjcro/sda/AbstractSqlRepository.java
@@ -12,28 +12,52 @@ public abstract class AbstractSqlRepository {
     private final SqlInvoker sqlInvoker;
     private final SqlModifier sqlModifier;
 
+    /**
+     * Constructs new abstract SQL repository with given {@link SqlInvoker} or {@link SqlModifier}
+     * and default current time provider.
+     *
+     * @param db Database connection to use, either {@link SqlInvoker} or {@link SqlModifier}.
+     */
     protected AbstractSqlRepository(SqlInvoker db) {
         this(db, null);
     }
 
+    /**
+     * Constructs new abstract SQL repository with given {@link SqlInvoker} or {@link SqlModifier}
+     *
+     * @param db           Database connection to use, either {@link SqlInvoker} or {@link SqlModifier}.
+     * @param timeProvider Current time provider, optional.
+     */
     protected AbstractSqlRepository(SqlInvoker db, Supplier<Instant> timeProvider) {
         this.sqlInvoker = Objects.requireNonNull(db, "db");
         this.sqlModifier = (db instanceof SqlModifier) ? (SqlModifier) db : null;
         this.timeProvider = timeProvider == null ? Instant::now : timeProvider;
     }
 
+    /**
+     * @return Current time.
+     */
     protected Instant getCurrentTime() {
         return timeProvider.get();
     }
 
+    /**
+     * @return Current time represented as epoch second (unix timestamp).
+     */
     protected long getCurrentEpochSecond() {
         return getCurrentTime().getEpochSecond();
     }
 
+    /**
+     * @return SQL invoker.
+     */
     protected SqlInvoker getSqlInvoker() {
         return sqlInvoker;
     }
 
+    /**
+     * @return SQL modifier.
+     */
     protected SqlModifier getSqlModifier() {
         if (sqlModifier == null) {
             throw new DatabaseException("no SQL modifier configured for " + getClass().getName());
