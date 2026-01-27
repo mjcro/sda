397e750637397b1eff81598a26a8b6a69f8bf204
Gotterdemarung <gotterdemarung@gmail.com>
2026-01-27 12:31:26 +0200

    :bug: Fixed instant handling

--- src/main/java/io/github/mjcro/sda/reflection/InstantSecondsTypeHandler.java
+++ src/main/java/io/github/mjcro/sda/reflection/InstantSecondsTypeHandler.java
@@ -3,7 +3,6 @@ package io.github.mjcro.sda.reflection;
 import org.jspecify.annotations.NonNull;
 
 import java.time.Instant;
-import java.time.ZoneOffset;
 import java.util.Objects;
 
 public class InstantSecondsTypeHandler extends AbstractSimpleColumnTypeHandler {
@@ -16,7 +15,7 @@ public class InstantSecondsTypeHandler extends AbstractSimpleColumnTypeHandler {
     protected ValueReader getValueReader(@NonNull String column) {
         return rs -> {
             long value = rs.getLong(column);
-            return rs.wasNull() ? null : Instant.ofEpochSecond(value).atZone(ZoneOffset.UTC);
+            return rs.wasNull() ? null : Instant.ofEpochSecond(value);
         };
     }
 }
