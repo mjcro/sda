df32f70e36333e804c985cd56f3d23f9e0d463d0
gotterdemarung <gotterdemarung@gmail.com>
2026-02-02 21:45:11 +0200

    :sparkles: Type handler for duration in seconds

---
+++ src/main/java/io/github/mjcro/sda/reflection/DurationSecondsTypeHandler.java
@@ -0,0 +1,21 @@
+package io.github.mjcro.sda.reflection;
+
+import org.jspecify.annotations.NonNull;
+
+import java.time.Duration;
+import java.util.Objects;
+
+public class DurationSecondsTypeHandler extends AbstractSimpleColumnTypeHandler {
+    @Override
+    protected boolean supports(@NonNull Class<?> clazz) {
+        return Objects.equals(Duration.class, clazz);
+    }
+
+    @Override
+    protected ValueReader getValueReader(@NonNull String column) {
+        return rs -> {
+            long value = rs.getLong(column);
+            return rs.wasNull() ? null : Duration.ofSeconds(value);
+        };
+    }
+}
